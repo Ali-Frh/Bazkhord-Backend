2cc69019ab2e419062f637f3c6ff66e39945cb55
Ali Frh <alifarhangifar@gmail.com>
2025-08-05 18:00:58 +0330

    feat(jwt): added jwt and required configuration

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
     "feedback",
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User Model
+AUTH_USER_MODEL = "app.User"
+
+# Django REST Framework Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
--- app/urls.py
+++ app/urls.py
@@ -17,5 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("feedback.urls"))]
+from .views import RegisterView
+
+urlpatterns = [
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/register/", RegisterView.as_view(), name="register"),
+    path("admin/", admin.site.urls),
+    path("api/", include("feedback.urls")),
+]
